f79a1d0d3ccc305e47c30e90b4f0d406b9d5db1b
Jeremy McPeak <jwmcpeak@gmail.com>
2024-01-25 22:46:21 -0600

    Episode 02

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login() {
-        return $this->ok('Hello, Login!');
+    public function login(ApiLoginRequest $request) {
+
+        return $this->ok($request->get('email'));
+    }
+
+    public function register() {
+        return $this->ok('register');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
