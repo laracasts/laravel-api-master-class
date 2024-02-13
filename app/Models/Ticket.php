e950a0d00f2b6bfa7946b85fc141ff37153955d5
Jeremy McPeak <jwmcpeak@gmail.com>
2024-02-12 21:38:52 -0600

    Revert "Episode 7"
    This reverts commit e304fd763185119318e8a0d442bdf62c3124b8ee.

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,13 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
     use HasFactory;
-
-    public function user(): BelongsTo {
-        return $this->belongsTo(User::class);
-    }
 }
